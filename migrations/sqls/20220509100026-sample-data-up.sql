644f0c79bfc88f59fde9b63d63e885a8d90a1691
Dev <dev@hello-art.io>
2022-05-09 19:02:22 +0900

    add sample data

---
+++ migrations/sqls/20220509100026-sample-data-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+INSERT INTO `users` (`id`, `uuid`, `name`, `email`, `phonenum`, `wallet_address`, `balance`,`withdraw_limit`,`privateKey`, `created`, `last_updated`)
+VALUES
+	(11, '6fc0a65e-c67b-4a15-811e-bd24e8e7ef5f', 'test11', 'silence14@hanmail.net', '01012344567', '0xa74e3a55f4058a100f89e4875b841f28dfc958b0',"0",100,'0x40d5000491721d234ae7ea29c8df582d6f3caa1e301e11b5578f8fd91eb0e566','2017-03-20 01:28:20', '2017-04-18 01:30:58');
